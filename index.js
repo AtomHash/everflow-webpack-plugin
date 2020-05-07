5590e632505b3f3bd456b000a44d0b8047b816af
Dylan Harty <me@dylanharty.com>
2020-05-06 20:31:19 -0600

    alpha branch

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const exec = require('child_process').exec;
 class EverflowWebpackPlugin {
 
     constructor(options =   {}) {
+        // Run scripts before dev server starts
         this.runner();
     }
 
     runner() {
         console.log('Everflow: Running scripts...');
+
+        // Run everflow script magic(Routes etc...)
         exec('node ./node_modules/@everflow-cli/tools/magic-routes.js', (err, stdout, stderr) => {
             if (stdout) process.stdout.write(stdout);
             if (stderr) process.stderr.write(stderr);
@@ -16,14 +19,16 @@ class EverflowWebpackPlugin {
 
     apply(compiler) {
 
+        // Make sure the load happy watcher does not compile newly generated files
         let initalRun = true;
-        compiler.hooks.invalid.tap('EverflowWebpack', (fileName, changeTime) => {
+
+        compiler.hooks.invalid.tap('EverflowWebpackPlugin', (fileName, changeTime) => {
             if (!initalRun)
             {
                 this.runner();
             }
         });
-        compiler.hooks.done.tap('EverflowWebpack', (stats ) => {
+        compiler.hooks.done.tap('EverflowWebpackPlugin', (stats ) => {
             initalRun = false;
         });
     }
